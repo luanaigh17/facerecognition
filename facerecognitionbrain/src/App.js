9c2267bc314c81892c99476a4bf4bd002501e4a1
Jeffrey Looney <jplooney17@gmail.com>
2023-02-13 21:23:03 -0600

    Initial Commit

---
+++ facerecognitionbrain/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+import ParticlesBg from 'particles-bg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#000000" num={200} type="cobweb" bg={true}  />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+  
+      </div>
+    );
+  }
+}
+
+export default App;
